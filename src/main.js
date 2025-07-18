a7ee1fedf613553884d54ed25a3f0a92e0c31959
InoriDokusha <163239186+Inori-dokusha@users.noreply.github.com>
2025-07-18 12:32:47 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,54 @@
+// fect data from data/data.json
+fetch("./src/data/data.json")
+  .then((response) => response.json())
+  .then((response) => {
+    getItems(response);
+  });
+
+function getItems(items) {
+  cards = "";
+  items.forEach((item) => {
+    cards += showItems(item);
+  });
+  const container = document.querySelector(".extensions");
+  container.innerHTML = cards;
+  onclick();
+}
+
+function onclick() {
+  document.addEventListener("click", (e) => {
+    if (e.target.className == "spinner" || e.target.className == "circle") {
+      click();
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  });
+}
+
+function click(toggle) {
+  toggle = document.querySelector(".click");
+  console.log(toggle);
+}
+
+function showItems(i) {
+  return `<div class="cards">
+            <div class="row">
+              <div class="row-1">
+                <div class="icon">
+                  <img src="${i.logo}">
+                </div>
+                <div class="info">
+                  <span>${i.name}</span>
+                  <p>${i.description}</p>
+                </div>
+              </div>
+              <div class="row-2">
+                <button class="remove">Remove</button>
+                <div class="spinner" id="${i.isActive}">
+                  <div class="circle"></div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+}
