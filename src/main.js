41a63effb9a27aa15ed8eea9ba328feedfe0fe0e
Inori-dokusha <inori.dokusha@gmail.com>
2025-08-05 04:50:18 +0700

    update by spck editor

--- src/main.js
+++ src/main.js
@@ -1,29 +1,39 @@
-// fect data from data/data.json
 fetch("./src/data/data.json")
-  .then((response) => response.json())
-  .then((response) => {
-    getItems(response);
-  });
+    .then((response) => response.json())
+    .then((response) => {
+        getItems(response);
+    });
 
 function getItems(items) {
-  cards = "";
-  items.forEach((item) => {
-    cards += showItems(item);
-  });
-  const container = document.querySelector(".extensions");
-  container.innerHTML = cards;
-  clicked();
+    cards = "";
+    items.forEach((item) => {
+        cards += showItems(item);
+    });
+    const container = document.querySelector(".extensions");
+    container.innerHTML = cards;
+    clicked();
+    toggleDarkmode();
 }
 
 function clicked() {
-  document.addEventListener("click", async function (e) {
-    const t = e.target.chill(".cirlce");
-    console.log(t);
-  });
+    const cards = document.querySelectorAll('.cards');
+    const circle = document.querySelectorAll(".circle");
+    cards.forEach(items => {
+        items.addEventListener("click", e => {
+            if (e.target.matches(".spinner") || e.target.matches(".circle")) {
+                circle.forEach((c) => {
+                    if (c == e.target || c.parentElement == e.target) {
+                        c.classList.toggle("non-active");
+                        c.parentElement.classList.toggle("bg-color");
+                    }
+                });
+            }
+        });
+    });
 }
 
 function showItems(i) {
-  return `<div class="cards">
+    return `<div class="cards">
             <div class="row">
               <div class="row-1">
                 <div class="icon">
@@ -42,4 +52,27 @@ function showItems(i) {
               </div>
             </div>
           </div>`;
+}
+
+// Darkmode
+function toggleDarkmode(path) {
+    path = {
+        dark: "assets/images/icon-moon.svg",
+        light: "assets/images/icon-sun.svg"
+    };
+    const toggle = document.querySelector(".toggle");
+    const icon = document.querySelector(".toggle .icon img");
+    toggle.addEventListener("click", async () => {
+        const attr = icon.getAttribute('src');
+        if (attr === path.dark) {
+            icon.setAttribute("src", path.light);
+            // Toggle theme
+            document.documentElement.setAttribute('data-theme', 'dark');
+            // Simpan preferensi
+            localStorage.setItem('theme', 'dark');
+        } else {
+            icon.setAttribute("src", path.dark)
+        }
+        
+    })
 }
